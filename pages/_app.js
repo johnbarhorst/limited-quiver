9d5d4a2d762ab5476df50c4a4922df3dd13f3a85
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-08-14 18:10:39 -0500

    layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { Nav } from '../components';
+import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Limited Quiver</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,13 @@
-import Head from 'next/head';
-import { Nav } from '../components';
-import styles from '../styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Limited Quiver</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Nav />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Limited Quiver Archery League
         </h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
