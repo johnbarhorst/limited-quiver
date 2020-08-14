deefac7b36543b5f26d024fc8a399c64bdfc1862
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-08-14 18:10:58 -0500

    testing with middlware

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,12 @@
-import dbConnect from '../../middleware/database';
+import nextConnect from 'next-connect';
+import middleware from 'middleware/middleware';
 
-dbConnect();
 
-export default async (req, res) => {
+const handler = nextConnect();
+handler.use(middleware);
+
+handler.get(async (req, res) => {
   res.json({ test: 'test' })
-}
+})
+
+export default handler;
