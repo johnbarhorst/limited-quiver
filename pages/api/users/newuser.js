c57f1abbfbeaffe83db41ed5e7e03a0fb7ae5775
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-08-16 15:20:47 -0500

    check for existing username

--- pages/api/users/newuser.js
+++ pages/api/users/newuser.js
@@ -4,13 +4,22 @@ import middleware from 'middleware/middleware';
 import User from 'models/UserModel';
 
 const handler = nextConnect();
-
 handler.use(middleware);
 
 handler.post(async (req, res) => {
   const data = JSON.parse(req.body);
-  const newUser = await User.create(data);
-  res.status(200).send(newUser);
+  const userExists = await User.findOne({ username: data.username });
+  if (userExists) {
+    return res.status(409).send({ success: false, message: `${data.username} already exists.` });
+  }
+
+  const newUser = new User(data);
+  newUser.save((saveErr, savedUser) => {
+    if (saveErr) {
+      res.status()
+    }
+    res.status(201).send({ success: true, data: savedUser });
+  })
 })
 
 export default handler;
