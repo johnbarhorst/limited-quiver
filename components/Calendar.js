ba1dd91b5b0b4ea3e62b73e929246083605fdfce
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-08-24 17:24:52 -0500

    removed all the +1 month mental overhead

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import {
   isDate,
   isSameDay,
@@ -12,15 +13,27 @@ import {
 } from 'utils/dateHelpers';
 
 export const Calendar = () => {
-  const today = new Date()
+  const [today, setToday] = useState(new Date());
   const [current, setCurrent] = useState(today);
-  const [month, setMonth] = useState(today.getMonth() + 1);
+  const [month, setMonth] = useState(today.getMonth());
   const [year, setYear] = useState(today.getFullYear());
-  const [calendar, setCalendar] = useState();
-
+  const [calendar, setCalendar] = useState(calendarBuilder(month, year));
+  // Build calendar based on selected month/year (init w/ today)
   useEffect(() => {
     setCalendar(calendarBuilder(month, year));
-  }, []);
+  }, [month, year]);
+
+  // Change 'today' if it becomes a bad time for mogwai feeding.
+  useEffect(() => {
+    const now = new Date();
+    const tomorrow = new Date().setHours(0, 0, 0, 0) + 24 * 60 * 60 * 1000;
+    const ms = tomorrow - now;
+    const dayTimeout = setTimeout(() => setToday(new Date()), ms);
+    return () => {
+      clearTimeout(dayTimeout);
+    }
+  })
+
 
   const goToPrevMonth = () => {
     const { prevMonth, prevMonthYear } = getPreviousMonth(month, year);
@@ -34,17 +47,60 @@ export const Calendar = () => {
     setYear(nextMonthYear);
   }
 
-  const getMonthName = month => Object.keys(CALENDAR_MONTHS)[
-    Math.max(0, Math.min(month - 1, 11))
-  ]
+  const goToNextYear = () => {
+    setYear(year => year + 1);
+  }
+
+  const goToPrevYear = () => {
+    setYear(year => year - 1);
+  }
+
+  const getMonthName = month => CALENDAR_MONTHS[month]
+
+  const selectCurrent = (date) => {
+    setCurrent(date.join("-"));
+  }
 
   return (
     <div>
       <h3>Calendar</h3>
-      <div>{Object.keys(WEEK_DAYS).map(day => <span key={day}>{WEEK_DAYS[day]} </span>)}</div>
       <div>{getMonthName(month)} {year}</div>
+      <DaysContainer>
+        <>
+          {WEEK_DAYS.map(day => <div key={day}><p>{day}</p></div>)}
+        </>
+        <>
+          {calendar.map((day, i) => <CalendarDay key={i} index={i} date={day} month={month} year={year} handleClick={selectCurrent} current={current} />)}
+        </>
+      </DaysContainer>
       <div><button onClick={() => goToPrevMonth()} >Prev</button><button onClick={() => goToNextMonth()}>Next</button></div>
-
+      <div><button onClick={() => goToPrevYear()}>Prev Year</button><button onClick={() => goToNextYear()} >Next Year</button></div>
     </div>
   )
 }
+
+
+const CalendarDay = ({ date, index, today, current, month, year, handleClick = f => f }) => {
+  const [, , d] = date;
+  const _date = new Date(year, month, d);
+  const isToday = isSameDay(_date, today);
+  const isCurrent = current && isSameDay(_date, current);
+  const inSameMonth = month && year && isSameMonth(_date, new Date([year, month].join("-")));
+
+  return (
+    <Day isToday={isToday} isCurrent={isCurrent} inSameMonth={inSameMonth} >
+      <button onClick={() => { console.log("is today", _date); handleClick(date) }} >{d}</button>
+    </Day>
+  )
+}
+
+const DaysContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  justify-content: center;
+  align-items: center;
+`;
+
+const Day = styled.div`
+  border: ${props => props.isToday ? '2px solid red' : '2px solid black'};
+`;
--- utils/dateHelpers.js
+++ utils/dateHelpers.js
@@ -1,36 +1,36 @@
 // (int) The current year
-export const THIS_YEAR = (new Date().getFullYear());
+export const THIS_YEAR = new Date().getFullYear();
 
 // (int) The current month starting from 1 - 12
 // 1 => January, 12 => December
-export const THIS_MONTH = (new Date().getMonth()) + 1;
+export const THIS_MONTH = new Date().getMonth();
 
 // Week days names and shortnames
-export const WEEK_DAYS = {
-  Sunday: "Sun",
-  Monday: "Mon",
-  Tuesday: "Tue",
-  Wednesday: "Wed",
-  Thursday: "Thu",
-  Friday: "Fri",
-  Saturday: "Sat"
-}
+export const WEEK_DAYS = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat"
+]
 
 // Calendar months names and shortnames
-export const CALENDAR_MONTHS = {
-  January: "Jan",
-  February: "Feb",
-  March: "Mar",
-  April: "Apr",
-  May: "May",
-  June: "Jun",
-  July: "Jul",
-  August: "Aug",
-  September: "Sep",
-  October: "Oct",
-  November: "Nov",
-  December: "Dec"
-}
+export const CALENDAR_MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 
 // Weeks displayed on calendar
 export const CALENDAR_WEEKS = 6;
@@ -43,10 +43,10 @@ export const zeroPad = (value, length) => {
 
 // (int) Number days in a month for a given year from 28 - 31
 export const getMonthDays = (month = THIS_MONTH, year = THIS_YEAR) => {
-  const months30 = [4, 6, 9, 11];
+  const months30 = [3, 5, 8, 0];
   const leapYear = year % 4 === 0;
 
-  return month === 2
+  return month === 1
     ? leapYear
       ? 29
       : 28
@@ -58,7 +58,7 @@ export const getMonthDays = (month = THIS_MONTH, year = THIS_YEAR) => {
 // (int) First day of the month for a given year from 1 - 7
 // 1 => Sunday, 7 => Saturday
 export const getMonthFirstDay = (month = THIS_MONTH, year = THIS_YEAR) => {
-  const firstOfMonth = (new Date(year, month - 1, 1).getDay()) + 1;
+  const firstOfMonth = new Date(year, month, 1).getDay();
   return firstOfMonth;
 }
 
@@ -75,13 +75,13 @@ export const isSameMonth = (date, basedate = new Date()) => {
 
   if (!(isDate(date) && isDate(basedate))) return false;
 
-  const basedateMonth = +(basedate.getMonth()) + 1;
+  const basedateMonth = basedate.getMonth();
   const basedateYear = basedate.getFullYear();
 
-  const dateMonth = +(date.getMonth()) + 1;
+  const dateMonth = date.getMonth();
   const dateYear = date.getFullYear();
 
-  return (+basedateMonth === +dateMonth) && (+basedateYear === +dateYear);
+  return (basedateMonth === dateMonth) && (basedateYear === dateYear);
 
 }
 
@@ -91,14 +91,14 @@ export const isSameDay = (date, basedate = new Date()) => {
   if (!(isDate(date) && isDate(basedate))) return false;
 
   const basedateDate = basedate.getDate();
-  const basedateMonth = +(basedate.getMonth()) + 1;
+  const basedateMonth = basedate.getMonth();
   const basedateYear = basedate.getFullYear();
 
   const dateDate = date.getDate();
-  const dateMonth = +(date.getMonth()) + 1;
+  const dateMonth = date.getMonth();
   const dateYear = date.getFullYear();
 
-  return (+basedateDate === +dateDate) && (+basedateMonth === +dateMonth) && (+basedateYear === +dateYear);
+  return (basedateDate === dateDate) && (basedateMonth === dateMonth) && (basedateYear === dateYear);
 
 }
 
@@ -110,8 +110,8 @@ export const getDateISO = (date = new Date) => {
 
   return [
     date.getFullYear(),
-    zeroPad(+date.getMonth() + 1, 2),
-    zeroPad(+date.getDate(), 2)
+    zeroPad(date.getMonth(), 2),
+    zeroPad(date.getDate(), 2)
   ].join('-');
 
 }
@@ -120,8 +120,8 @@ export const getDateISO = (date = new Date) => {
 // For example: getPreviousMonth(1, 2000) => {month: 12, year: 1999}
 // while: getPreviousMonth(12, 2000) => {month: 11, year: 2000}
 export const getPreviousMonth = (month, year) => {
-  const prevMonth = (month > 1) ? month - 1 : 12;
-  const prevMonthYear = (month > 1) ? year : year - 1;
+  const prevMonth = month > 0 ? month - 1 : 11;
+  const prevMonthYear = month > 0 ? year : year - 1;
 
   return { prevMonth, prevMonthYear };
 }
@@ -130,8 +130,8 @@ export const getPreviousMonth = (month, year) => {
 // For example: getNextMonth(1, 2000) => {month: 2, year: 2000}
 // while: getNextMonth(12, 2000) => {month: 1, year: 2001}
 export const getNextMonth = (month, year) => {
-  const nextMonth = (month < 12) ? month + 1 : 1;
-  const nextMonthYear = (month < 12) ? year : year + 1;
+  const nextMonth = month < 11 ? month + 1 : 1;
+  const nextMonthYear = month < 11 ? year : year + 1;
 
   return { nextMonth, nextMonthYear };
 }
@@ -151,14 +151,13 @@ export const calendarBuilder = (month = THIS_MONTH, year = THIS_YEAR) => {
   // Get number of days to be displayed from previous and next months
   // These ensure a total of 42 days (6 weeks) displayed on the calendar
 
-  const daysFromPrevMonth = monthFirstDay - 1;
+  const daysFromPrevMonth = monthFirstDay;
   const daysFromNextMonth = (CALENDAR_WEEKS * 7) - (daysFromPrevMonth + monthDays);
 
   // Get the previous and next months and years 
 
   const { prevMonth, prevMonthYear } = getPreviousMonth(month, year);
   const { nextMonth, nextMonthYear } = getNextMonth(month, year);
-  console.log("p m y", prevMonthYear)
   // Get number of days in previous monthDays
   const prevMonthDays = getMonthDays(prevMonth, prevMonthYear);
 
