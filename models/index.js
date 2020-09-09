05ad9aa3fcc554e877fb21405159fa74457633fd
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-09-09 18:44:15 -0500

    data sources for caching

---
+++ models/index.js
@@ -0,0 +1,17 @@
+import User from './UserModel';
+import Event from './EventModel';
+import League from './LeagueModel';
+
+
+// Apollo recommends using models and data sources within context for fetching data and
+// DB stuff instead of within resolvers. Then your resolver can just access context/datasource
+// for the methods you want to accomplish. At this stage I feel like that is just one more level of 
+// abstraction, but here is a note for future me if I want to revisit this way of life.
+
+const models = {
+  User,
+  Event,
+  League
+}
+
+export default models;
