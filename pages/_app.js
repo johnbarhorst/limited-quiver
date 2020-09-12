34b4aa81df08b883158f8707e5d38378bda1dd4f
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-09-11 19:01:02 -0500

    normalize and such

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { Navigation } from '../components';
+import 'styles/normalize.css';
 import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
