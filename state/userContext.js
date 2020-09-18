4691eec0e496fb8f172ad5497d83fe123dafb8af
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-09-17 22:20:09 -0500

    user context created

---
+++ state/userContext.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext({
+  isLoggedIn: false,
+  user: null
+});
+
+export const UserContextWrapper = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <UserContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        user: user,
+        setUser: data => setUser(data),
+        setIsLoggedIn: bool => setIsLoggedIn(bool)
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUserContext = () => useContext(UserContext);
