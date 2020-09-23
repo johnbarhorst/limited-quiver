512c27e7967fdc253c31a1233f351e012b4f32ce
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-09-23 16:57:51 -0500

    seo lighthouse improvement

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ const AppToWrap = ({ Component, pageProps }) => {
     <>
       <Head>
         <title>Limited Quiver</title>
+        <meta name="description" content="Score keeping resource for archery leagues of all skill levels." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navigation />
