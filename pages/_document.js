1980466aeb8227798e4588451d95a4c26d5e5a82
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-09-23 16:56:57 -0500

    lang att for accessibility

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 // For server side rendering of styled components.
@@ -26,7 +26,17 @@ class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
 
