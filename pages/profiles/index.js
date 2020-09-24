6d66c6c028139e99ec0ee1888cb05102588d6ab1
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-09-24 18:25:53 -0500

    logout button on user page

--- pages/profiles/index.js
+++ pages/profiles/index.js
@@ -1,12 +1,26 @@
 import { useAppContext } from 'state';
+import { useMutation, gql } from '@apollo/client';
+
+
+const LOGOUT_USER = gql`
+mutation LogoutUser {
+  logoutUser 
+}`;
 
 const ProfilesHome = () => {
-  const { user } = useAppContext()
+  const { user, setUser } = useAppContext();
+  const [logout, { data, error, loading }] = useMutation(LOGOUT_USER);
+
+  const handleLogout = () => {
+    logout();
+    setUser(null);
+  }
   return (
     <div>
       <h1>
         {user ? user.username : "User Profiles"}
       </h1>
+      <button onClick={() => handleLogout()} >Logout!</button>
     </div>
   )
 }
