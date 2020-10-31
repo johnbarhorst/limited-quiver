35fdb29829f8eb709a17e5d162e9ce961ca4e724
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-10-31 15:22:55 -0500

    quick test endpoint. remove later

---
+++ pages/api/test.js
@@ -0,0 +1,18 @@
+import User from 'models/UserModel';
+import dbConnect from 'utils/dbConnect';
+
+const test = async (req, res) => {
+  try {
+    await dbConnect();
+    const user = await User.findById("5f988cdb30a3097309ebdbd6");
+    console.log(user);
+    res.status(200).json(user);
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Error!", error: e })
+  }
+
+}
+
+export default test;
