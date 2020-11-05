2a9320322716a022f4ecf41580a1fde3a999598a
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-11-04 19:06:52 -0600

    login/out in one file

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -5,8 +5,16 @@ import middleware from 'middleware/middleware';
 const handler = nextConnect();
 
 handler.use(middleware);
+
+// login
 handler.post(passport.authenticate('local'), (req, res) => {
   res.json(req.user);
+});
+
+// logout
+handler.delete((req, res) => {
+  req.logOut();
+  res.status(204).end();
 });
 
 export default handler;
