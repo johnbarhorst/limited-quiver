0bf0c984729aa4417ec9c1f43ab431786552ef6a
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-11-06 08:47:04 -0600

    not using that just yet, not set up

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,9 +1,9 @@
 import nextConnect from 'next-connect';
 import middleware from 'middleware/middleware';
-import { extractUser } from 'lib/api-helpers';
 
+const handler = nextConnect();
 handler.use(middleware);
 
-handler.get((req, res) => res.json(extractUser(req)));
+handler.get((req, res) => res.json(req.user));
 
 export default handler;
