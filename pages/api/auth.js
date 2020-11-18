3f9dea4746db7a39184989a165a67e45f524e844
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-11-17 20:22:39 -0600

    some cookie work

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,5 +1,5 @@
 import nextConnect from 'next-connect';
-import { serialize } from 'cookie';
+import { setTokenCookie, removeTokenCookie } from 'lib';
 import passport from 'lib/passport-config';
 import middleware from 'middleware/middleware';
 
@@ -9,18 +9,14 @@ handler.use(middleware);
 
 // login
 handler.post(passport.authenticate('local'), (req, res) => {
-  // Second cookie here for reauthorization and getting user data.
-  // This feels wrong...
-  res.setHeader('Set-Cookie', serialize('LQ_USER', "youareloggedinIguess", {
-    httpOnly: false,
-    maxAge: 60 * 60 * 24 * 7
-  }));
+  setTokenCookie(res, "logged in");
   res.json(req.user);
 });
 
 // logout
 handler.delete((req, res) => {
   req.logOut();
+  removeTokenCookie(res);
   res.status(204).end();
 });
 
