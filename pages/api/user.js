d75c7f4b92b33d96320111912b6aa74acfa75e2d
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-11-18 18:09:33 -0600

    fallback just in case

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,11 +1,16 @@
 import nextConnect from 'next-connect';
 import middleware from 'middleware/middleware';
+import { removeTokenCookie } from 'lib';
 
 const handler = nextConnect();
 handler.use(middleware);
 
 handler.get((req, res) => {
   if (!req.user) {
+    // I don't know if there's a way this endpoint would get hit without the cookie being there,
+    // but if there is no user session, lets make sure we pull that cookie off.
+
+    removeTokenCookie(res);
     const error = new Error("Not authorized!");
     error.status = 403;
     throw error;
