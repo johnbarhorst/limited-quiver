abdee71f23da59c347b83281075b67e99d1ac7fc
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-01-06 13:06:31 -0600

    not using that cookie anymore

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -10,14 +10,12 @@ handler.use(middleware);
 // login
 handler.post(passport.authenticate('local'), (req, res) => {
   // TODO: this feels fragile. There may be a way for the req to lose .user and this cookie to still be there?
-  setTokenCookie(res, "logged in");
   res.json(req.user);
 });
 
 // logout
 handler.delete((req, res) => {
   req.logOut();
-  removeTokenCookie(res);
   res.status(204).end();
 });
 
