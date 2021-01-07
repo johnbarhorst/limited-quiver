931c2d6121efb687097982c6a83641a2c899fbc8
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-01-07 17:17:16 -0600

    fleshing it out a bit

--- components/EventSmallDisplay.js
+++ components/EventSmallDisplay.js
@@ -1,8 +1,19 @@
+import { EventCard_Small } from 'elements';
+import { convertISODate } from 'lib'
+
 export const EventSmallDisplay = ({ event }) => {
+  const eventDate = convertISODate(event.startDate)
   return (
-    <div>
-      <h5>{event.name}</h5>
-      <p>Creator: {event.createdBy.username}</p>
-    </div>
+    <EventCard_Small>
+      <div>
+        <h3>{event.name}</h3>
+        <p>Created by: {event.createdBy.username}</p>
+      </div>
+      <div>
+        <p>Rounds: {event.rounds}</p>
+        <p>SPR: {event.shotsPer}</p>
+        <p>{eventDate}</p>
+      </div>
+    </EventCard_Small>
   )
 }
