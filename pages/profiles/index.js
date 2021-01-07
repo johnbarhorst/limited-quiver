37e34d8919f67fd9cc1d44e9dc485d7358a6a1f1
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-01-07 17:17:49 -0600

    logout moved to own button

--- pages/profiles/index.js
+++ pages/profiles/index.js
@@ -1,16 +1,12 @@
+import nextConnect from 'next-connect';
+import middleware from 'middleware/middleware';
 import { useUser } from 'hooks';
-import { Login, SignUpModal } from 'components';
+import { Login, SignUp, LogoutButton } from 'components';
 
 
-const ProfilesHome = () => {
-  const { user, userIsLoading, userIsError, mutate } = useUser();
+const ProfilesHome = (props) => {
+  const { user, userIsLoading, userIsError, mutate } = useUser(props.user);
 
-  const handleLogout = async () => {
-    await fetch('/api/auth', {
-      method: 'DELETE',
-    });
-    mutate(null);
-  }
   return (
     <main>
       <h1>
@@ -18,11 +14,11 @@ const ProfilesHome = () => {
       </h1>
       <div>
         {user ?
-          <button onClick={() => handleLogout()} >Logout</button>
+          <LogoutButton />
           :
           <>
             <Login />
-            <SignUpModal />
+            <SignUp />
           </>
         }
       </div>
@@ -31,3 +27,27 @@ const ProfilesHome = () => {
 }
 
 export default ProfilesHome;
+
+export async function getServerSideProps({ req, res }) {
+  // tap into the middleware
+  const handler = nextConnect();
+  handler.use(middleware);
+  try {
+    await handler.run(req, res);
+  } catch (error) {
+    // TODO Handle errors
+    console.log(error)
+  }
+  if (!req.user) {
+    return {
+      props: {
+        user: null
+      }
+    }
+  }
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(req.user))
+    }
+  }
+}
