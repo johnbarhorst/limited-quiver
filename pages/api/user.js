ee6a86fb63375a704cb01070c2e1512debb13e1d
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-01-27 17:04:36 -0600

    more organized

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,11 +1,11 @@
 import nextConnect from 'next-connect';
 import middleware from 'middleware/middleware';
-import { getUser, createUser } from 'lib';
+import { getUser, createUser } from 'lib/userHandlers';
 
 const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(getUser);
-// handler.post(createUser);
+handler.post(createUser);
 
 export default handler;
