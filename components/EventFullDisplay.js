705afe78f435fe24884051b4eedeefd93a92c9b7
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-01-27 16:58:02 -0600

    fleshing out a bit

--- components/EventFullDisplay.js
+++ components/EventFullDisplay.js
@@ -4,10 +4,21 @@ export const EventFullDisplay = ({ eventId }) => {
   const { event, eventIsError, eventIsLoading } = useEvent(eventId);
 
   if (eventIsLoading) return (<h3>Loading event...</h3>)
-
+  if (eventIsError) return (<h3>We encountered an error while loading this event.</h3>)
   return (
     <>
       <h5>{event.name}</h5>
+      <div>
+        <ul>
+          <li>Created by: {event.createdBy.username}</li>
+          <li>Rounds: {event.rounds}</li>
+          <li>Shots Per Round: {event.shotsPer}</li>
+          <li>{event.private ? "This is a private event" : "This event is open to everyone"}</li>
+          <li>Maximum Participants: {event.participantCap}</li>
+        </ul>
+
+
+      </div>
     </>
   )
 }
