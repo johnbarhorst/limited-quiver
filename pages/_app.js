fa55a44811080130e7f862ee36467bd8fbe716e0
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-02-24 17:01:38 -0600

    loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 import { ToastContextWrapper } from 'state';
 import 'styles/normalize.css';
 import 'styles/globals.css';
 import 'styles/style.css';
+import 'styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 
 
