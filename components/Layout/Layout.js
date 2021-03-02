528d52334f2ebe83bf508b94db2bc0779b6b46c4
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-02 14:19:42 -0600

    Title change functionality

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,29 +1,36 @@
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 
 import { Navigation, ToastModule } from 'components';
 
-export const Layout = ({ children }) => {
+export const Layout = ({ children, title }) => {
   return (
     <>
       <Head>
-        <title>Limited Quiver</title>
+        <title>Limited Quiver{title ? ` | ${title}` :  null}</title>
         <meta name="description" content="Score keeping resource for archery leagues of all skill levels." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ToastModule position={"BOTTOM_RIGHT"} />
+      <ToastModule position={'BOTTOM_RIGHT'} />
       <Navigation />
       <Wrapper>
         {children}
       </Wrapper>
     </>
-  )
-}
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.any,
+  title: PropTypes.string
+};
 
 const Wrapper = styled(motion.main)`
   background: ${props => props.theme.colors.bgPrimary};
   padding-bottom: ${props => props.theme.sizes.navHeightMobile}; 
   color: ${props => props.theme.colors.fontPrimary};
 `;
+
