34944fd8a2fa1687804e11bac467305e40508fd6
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-03 15:45:53 -0600

    title functionality

--- pages/profiles/index.js
+++ pages/profiles/index.js
@@ -6,53 +6,53 @@ import { Layout, Login, SignUp, LogoutButton } from 'components';
 
 
 const ProfilesHome = (props) => {
-	const { user } = useUser(props.user);
+  const { user } = useUser(props.user);
 
-	return (
-		<Layout>
-			<h1>
-				{user ? user.username : 'User Profiles'}
-			</h1>
-			<div>
-				{user ?
-					<LogoutButton />
-					:
-					<>
-						<Login />
-						<SignUp />
-					</>
-				}
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout title={user?.username}>
+      <h1>
+        {user ? user.username : 'User Profiles'}
+      </h1>
+      <div>
+        {user ?
+          <LogoutButton />
+          :
+          <>
+            <Login />
+            <SignUp />
+          </>
+        }
+      </div>
+    </Layout>
+  );
 };
 
 export default ProfilesHome;
 
 export async function getServerSideProps({ req, res }) {
-	// tap into the middleware
-	const handler = nextConnect();
-	handler.use(middleware);
-	try {
-		await handler.run(req, res);
-	} catch (error) {
-		// TODO Handle errors
-		console.log(error);
-	}
-	if (!req.user) {
-		return {
-			props: {
-				user: null
-			}
-		};
-	}
-	return {
-		props: {
-			user: JSON.parse(JSON.stringify(req.user))
-		}
-	};
+  // tap into the middleware
+  const handler = nextConnect();
+  handler.use(middleware);
+  try {
+    await handler.run(req, res);
+  } catch (error) {
+    // TODO Handle errors
+    console.log(error);
+  }
+  if (!req.user) {
+    return {
+      props: {
+        user: null
+      }
+    };
+  }
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(req.user))
+    }
+  };
 }
 
 ProfilesHome.propTypes = {
-	user: PropTypes.object
+  user: PropTypes.object
 };
