81ca9e75c7bee5b9a29cf9c8f87e19bc7926efa9
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-03 15:42:31 -0600

    just need full page view for now

--- components/EventSmallDisplay.js
+++ components/EventSmallDisplay.js
@@ -1,28 +1,36 @@
+import PropTypes from 'prop-types';
 import Link from 'next/link';
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
 import { EventCard_Small } from 'elements';
-import { convertISODate } from 'lib';
+import { GiArcher } from 'react-icons/gi';
 
-export const EventSmallDisplay = ({ event, clickHandler = f => f }) => {
-  const eventDate = convertISODate(event.startDate)
+export const EventSmallDisplay = ({ event }) => {
   return (
-    <Wrapper onClick={clickHandler} >
+    <Link href={`/events/${event._id}`}>
       <EventCard_Small>
         <div>
           <h3>{event.name}</h3>
           <p>Created by: {event.createdBy.username}</p>
         </div>
-        <div>
-          <p>Event Date: {eventDate}</p>
-        </div>
+        <ul>
+          <li><GiArcher /> {event.participants.length}/{event.participantCap}</li>
+          <li>{event.private ? 'Private' : 'Public'}</li>
+        </ul>
       </EventCard_Small>
-    </Wrapper>
-  )
-}
+    </Link>
+  );
+};
 
-const Wrapper = styled(motion.button)`
-  background: none;
-  border: none; 
-  padding: 0;
-`;
+EventSmallDisplay.propTypes = {
+  event: PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    shotsPerRound: PropTypes.number,
+    participantCap: PropTypes.number,
+    participants: PropTypes.arrayOf(PropTypes.string),
+    private: PropTypes.bool,
+    createdBy: PropTypes.shape({
+      username: PropTypes.string
+    }),
+  }),
+};
+
