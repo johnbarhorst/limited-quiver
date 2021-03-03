99d80e3c05607940b203fd12e48d64d245943dfd
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-03 15:46:03 -0600

    layout stuff

--- components/EventFullDisplay.js
+++ components/EventFullDisplay.js
@@ -1,4 +1,7 @@
+import PropTypes from 'prop-types';
 import { useEvent } from 'hooks';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 export const EventFullDisplay = ({ eventId, initialData }) => {
   const { event, eventIsError, eventIsLoading } = useEvent(eventId, initialData);
@@ -7,22 +10,32 @@ export const EventFullDisplay = ({ eventId, initialData }) => {
     <>
       <h5>Loading event...</h5>
     </>
-  )
-  if (eventIsError) return (<h5>We encountered an error while loading this event.</h5>)
+  );
+  if (eventIsError) return (<h5>We encountered an error while loading this event.</h5>);
   return (
     <>
       <h5>{event.name}</h5>
-      <div>
+      <EventStyles>
         <ul>
           <li>Created by: {event.createdBy.username}</li>
           <li>Rounds: {event.rounds}</li>
           <li>Shots Per Round: {event.shotsPer}</li>
-          <li>{event.private ? "This is a private event" : "This event is open to everyone"}</li>
+          <li>{event.private ? 'This is a private event' : 'This event is open to everyone'}</li>
           <li>Maximum Participants: {event.participantCap}</li>
         </ul>
-
-
-      </div>
+      </EventStyles>
     </>
-  )
-}
+  );
+};
+
+const EventStyles = styled(motion.section)`
+  ul {
+    list-style: none;
+    
+  }
+`;
+
+EventFullDisplay.propTypes = {
+  eventId: PropTypes.string,
+  initialData: PropTypes.object
+};
