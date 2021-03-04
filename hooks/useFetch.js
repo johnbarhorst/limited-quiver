750ff99b58e9cd2e2bb5cdec7c250bed896b62c2
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-03 21:29:52 -0600

    Lets throw more logs on this fire.

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -2,7 +2,10 @@ import useSWR from 'swr';
 import { fetcher } from 'lib/api-helpers';
 
 export const useFetch = ({ url, initialData }) => {
+  console.log('Hitting useFetch');
   const { data, error, mutate } = useSWR(url, fetcher, initialData ? initialData : null);
+  console.log('useFetch data', data);
+  console.log('useFetch error', error);
   return {
     data,
     error,
