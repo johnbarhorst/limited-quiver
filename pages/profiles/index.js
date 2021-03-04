807f44cc187c27ddca42467be8b4560a2b4015c5
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-03 21:12:59 -0600

    gSSP test for 504

--- pages/profiles/index.js
+++ pages/profiles/index.js
@@ -5,8 +5,8 @@ import { useUser } from 'hooks';
 import { Layout, Login, SignUp, LogoutButton } from 'components';
 
 
-const ProfilesHome = (props) => {
-  const { user } = useUser(props.user);
+const ProfilesHome = () => {
+  const { user } = useUser();
 
   return (
     <Layout title={user?.username}>
@@ -29,29 +29,29 @@ const ProfilesHome = (props) => {
 
 export default ProfilesHome;
 
-export async function getServerSideProps({ req, res }) {
-  // tap into the middleware
-  const handler = nextConnect();
-  handler.use(middleware);
-  try {
-    await handler.run(req, res);
-  } catch (error) {
-    // TODO Handle errors
-    console.log(error);
-  }
-  if (!req.user) {
-    return {
-      props: {
-        user: null
-      }
-    };
-  }
-  return {
-    props: {
-      user: JSON.parse(JSON.stringify(req.user))
-    }
-  };
-}
+// export async function getServerSideProps({ req, res }) {
+//   // tap into the middleware
+//   const handler = nextConnect();
+//   handler.use(middleware);
+//   try {
+//     await handler.run(req, res);
+//   } catch (error) {
+//     // TODO Handle errors
+//     console.log(error);
+//   }
+//   if (!req.user) {
+//     return {
+//       props: {
+//         user: null
+//       }
+//     };
+//   }
+//   return {
+//     props: {
+//       user: JSON.parse(JSON.stringify(req.user))
+//     }
+//   };
+// }
 
 ProfilesHome.propTypes = {
   user: PropTypes.object
