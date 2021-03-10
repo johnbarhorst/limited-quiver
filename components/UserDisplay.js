e24c5420bb612c4465eb037ab695055aa95ce802
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-10 13:15:45 -0600

    prop types

--- components/UserDisplay.js
+++ components/UserDisplay.js
@@ -1,7 +1,15 @@
+import PropTypes from 'prop-types';
+
 export const UserDisplay = ({ user }) => {
   return (
     <div>
       <h3>{user.username}</h3>
     </div>
-  )
-}
+  );
+};
+
+UserDisplay.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string
+  })
+};
