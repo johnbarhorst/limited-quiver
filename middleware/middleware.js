b00544347c6b849e2f352cd6db3da3093fc030a8
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-10 10:02:37 -0600

    initial participant schema

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,15 +4,18 @@ import sessionMiddleware from './session';
 import { dbConnect } from 'lib';
 
 /* eslint-disable no-unused-vars */
-// We aren't using these here. But it initializes the schemas into Mongoose.
+// We aren't actually using these here. But it seems to initialize the schemas into Mongoose.
 // Otherwise, we get an error doing things like user.populate()
 // Feels weird, there may be a 'better' way. But head + wall cure right now.
 import Event from 'models/EventModel';
 import User from 'models/UserModel';
 import League from 'models/LeagueModel';
+import Participant from 'models/ParticipantModel';
 
 const middleware = nextConnect();
 
+// Next examples show db connection chained after sessionMiddleware.
+// But for us, on Vercel, if you don't load the db first, we get sporadic time outs on our serverless funcs
 middleware
   .use(async (req, res, next) => {
     await dbConnect('Middleware.js');
---
+++ models/ParticipantModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const ParticipantSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'A user id was not provided to new Participant.user'],
+  },
+  event: {
+    type: Schema.Types.ObjectId,
+    ref: 'Event',
+    required: [true, 'An event id was not provided to new Participant.event'],
+  },
+  scores: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Score'
+  }],
+  // TODO: Permissions and or roles
+});
+
+const Participant = mongoose.models.Participant || mongoose.model('Participant', ParticipantSchema);
+export default Participant;
