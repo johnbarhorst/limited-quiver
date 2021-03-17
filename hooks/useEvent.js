6c4e96139458c7e65232bf78b08b2ebe558e1b5b
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-16 20:44:42 -0500

    will need mutate after updates

--- hooks/useEvent.js
+++ hooks/useEvent.js
@@ -2,14 +2,14 @@ import useSWR from 'swr';
 import { fetcher } from 'lib';
 
 export const useEvent = (eventID, initialData) => {
-  const { data, error } = useSWR(
-    eventID ? `/api/events/getEvent?eventId=${eventID}` : null,
+  const { data, error, mutate } = useSWR(
+    eventID ? `/api/event?eventId=${eventID}` : null,
     fetcher,
     initialData ? { initialData } : null);
-
   return {
     event: data,
     eventIsLoading: !error && !data,
-    eventIsError: error
+    eventIsError: error,
+    mutate
   };
 };
